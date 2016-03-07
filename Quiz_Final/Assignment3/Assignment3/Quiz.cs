3d9a14743a65f2daa5abc4d3e949edbc0d397cfe
Jared Everett <s0739107@SMUNET.SMU.CA>
2016-03-06 20:01:19 -0400

    added quiz form

---
+++ Quiz_Final/Assignment3/Assignment3/Quiz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataLib;
+
+namespace Assignment3
+{
+    public partial class Quiz : Form
+    {
+        private int questionCount = 5;
+
+        DBAccess db;
+        List<Question> questions = new List<Question>();
+
+        public Quiz(DBAccess db)
+        {
+            InitializeComponent();
+            this.db = db;
+        }
+
+        private void Quiz_Load(object sender, EventArgs e)
+        {
+            // select random questions for this quiz
+            Random r = new Random();
+            for (int i = 0;i<questionCount;i++)
+            {                
+                questions.Add(db.questions[r.Next(db.questions.Count)]);
+            }
+        }
+    }
+}
